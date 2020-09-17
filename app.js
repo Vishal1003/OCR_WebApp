8b274a47d4ed1c97e72f41c86f664486a664bed8
Vishal1003 <vishalj.ec18@nsut.ac.in>
2020-09-18 01:55:03 +0530

    basic OCR completed

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const { TesseractWorker } = require('tesseract.js')
+const worker = new TesseractWorker();
+
+
+// multer disk storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage }).single("avatar");
+app.set("view engine", "ejs");
+
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        // console.log(req.file);
+        fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+            if(err) 
+               return console.log('This is your error', err);
+            
+            worker
+            .recognize(data, "eng", {tessjs_create_pdf : '1'})
+            .progress(progress => {
+                console.log(progress);
+            })
+            .then(result => {
+                res.send(result.text);
+            })
+            .finally(() => worker.terminate());
+        })
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server up and running on port ${PORT}`)
+})
